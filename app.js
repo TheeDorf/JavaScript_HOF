752e1f9f630f6f164415e8a24122c60cfa025442
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-20 09:29:11 -0700

    complete exercises

--- app.js
+++ app.js
@@ -1,4 +1,81 @@
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+function plus(number){
+    return function(plusNumber)
+        {return plusNumber + number}
+}
+let = plus15 = plus(15);
+console.log(plus15(10));
+
+
+
+
+// ex 2
+
+let users = [
+  {
+    name: "Frodo",
+    age: 50,
+    score: 85,
+    isActive: false,
+  },
+  {
+    name: "Sam",
+    age: 38,
+    score: 94,
+    isActive: true,
+  },
+  {
+    name: "Merry",
+    age: 36,
+    score: 82,
+    isActive: true,
+  },
+  {
+    name: "Pippin",
+    age: 26,
+    score: 77,
+    isActive: false,
+  },
+];
+
+users.forEach ((hobbit)=>{
+    console.log(hobbit.name);
+});
+
+// ex3
+
+const nameAndScore = users.map((users)=>{
+    return {
+        name: users.name,
+        score: users.score,
+    };
+});
+console.log(nameAndScore)
+
+// ex4
+
+const active = users.filter((users)=>{
+    return users.isActive == true
+});
+
+console.log(active);
+
+// ex 5
+
+const sort = users.sort((a,b)=>{
+   return b.score - a.score;
+});
+
+console.log(sort)
+// ex 6
+
+const scoreSum = users.reduce((total, users)=>{
+    return total + users.score;
+},0);
+
+const averageScore = scoreSum / users.length;
+
+console.log(scoreSum);
+console.log(averageScore);
